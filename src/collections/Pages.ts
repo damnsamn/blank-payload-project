bf21d38114530c68146f2d32412e23242e7460bc
Sam Barnett <sam@humaan.com.au>
2025-03-06 16:38:39 +0800

    reproduction

---
+++ src/collections/Pages.ts
@@ -0,0 +1,73 @@
+import { CollectionConfig } from 'payload'
+
+export const Pages: CollectionConfig = {
+  slug: 'pages',
+  // versions: {
+  //   drafts: {
+  //     validate: true,
+  //   },
+  // },
+  fields: [
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          label: 'General',
+          fields: [
+            {
+              label: 'Collapsible A',
+              type: 'collapsible',
+              fields: [
+                {
+                  type: 'text',
+                  name: 'contentA',
+                  required: true,
+                },
+                {
+                  label: 'Collapsible B',
+                  type: 'collapsible',
+                  fields: [
+                    {
+                      type: 'text',
+                      name: 'contentB',
+                      required: true,
+                    },
+                  ],
+                },
+                {
+                  type: 'group',
+                  name: 'groupA',
+                  fields: [
+                    {
+                      type: 'text',
+                      name: 'contentC',
+                      required: true,
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              label: 'Collapsible C',
+              type: 'collapsible',
+              required: true,
+              fields: [
+                {
+                  type: 'group',
+                  name: 'groupB',
+                  fields: [
+                    {
+                      type: 'text',
+                      name: 'contentD',
+                      required: true,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
